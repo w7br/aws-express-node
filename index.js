4c08f0fe536e8f3b3d906b0d1bab76da30fcda18
Allexus Technology <49105948+allexus-technology@users.noreply.github.com>
2022-06-20 13:13:50 -0300

    updated

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ app.get('/', (request, response) => {
     return response.json({message: 'Servidor rodando na AWS com NODE e EXPRESS, finalmente hahaha'});
 })
 
+app.get('/atualizou', (request, response) => {
+    return response.json({message: 'Updated'});
+})
+
+app.get('/teste', (request, response) => {
+    const {name, date} = request.body;
+    return response.json({name, date});
+})
+
 app.listen(3333);
