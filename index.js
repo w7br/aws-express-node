7cc90dca627db3874cc5aba602eb761972347f7b
Allexus Technology <49105948+allexus-technology@users.noreply.github.com>
2022-06-20 12:46:47 -0300

    updatede

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({message: 'Servidor rodando na AWS, finalmente hahaha'});
+    return response.json({message: 'Servidor rodando na AWS com NODE e EXPRESS, finalmente hahaha'});
 })
 
 app.listen(3333);
